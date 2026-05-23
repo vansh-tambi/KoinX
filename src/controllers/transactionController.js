7b2fc2dc5313df4b010ffb8a362ed0a2257fa371
vansh-tambi <23u02084@iiitbhopal.ac.in>
2026-05-23 16:20:03 +0530

    feat: implement transaction controller for CRUD operations

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,28 +1,66 @@
 import transactionService from '../services/transactionService.js';
 
-const getAll = async (req, res) => {
-  const result = await transactionService.getAll();
-  res.json(result);
+const getAll = async (req, res, next) => {
+  try {
+    const result = await transactionService.getAll();
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const create = async (req, res) => {
-  const result = await transactionService.create(req.body);
-  res.status(201).json(result);
+const create = async (req, res, next) => {
+  try {
+    const result = await transactionService.create(req.body);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const getById = async (req, res) => {
-  const result = await transactionService.getById(req.params.id);
-  res.json(result);
+const getById = async (req, res, next) => {
+  try {
+    const result = await transactionService.getById(req.params.id);
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        error: { message: `Transaction not found with ID: ${req.params.id}` }
+      });
+    }
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const update = async (req, res) => {
-  const result = await transactionService.update(req.params.id, req.body);
-  res.json(result);
+const update = async (req, res, next) => {
+  try {
+    const result = await transactionService.update(req.params.id, req.body);
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        error: { message: `Transaction not found with ID: ${req.params.id}` }
+      });
+    }
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const remove = async (req, res) => {
-  await transactionService.remove(req.params.id);
-  res.sendStatus(204);
+const remove = async (req, res, next) => {
+  try {
+    const result = await transactionService.remove(req.params.id);
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        error: { message: `Transaction not found with ID: ${req.params.id}` }
+      });
+    }
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default { getAll, create, getById, update, remove };
