905ce52ccae184325a33232a23b2009b8ba86e64
vansh-tambi <23u02084@iiitbhopal.ac.in>
2026-05-23 16:38:17 +0530

    feat: add reconciliation route index and remove unused multer dependency

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
 import { Router } from 'express';
-import transactionRoutes from './transactionRoutes.js';
 import reconciliationRoutes from './reconciliationRoutes.js';
 
 const router = Router();
 
-router.use('/transactions', transactionRoutes);
 router.use('/reconciliation', reconciliationRoutes);
 
 export default router;
